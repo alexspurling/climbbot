1db75cb2910ee68089cf7f9ad56fd72e45639b96
Alex Spurling <alex.spurling@playfish.com>
2012-11-15 20:32:28 +0000

    Implementing conversion algorithm using dictionaries

--- climbbot.py
+++ climbbot.py
@@ -10,16 +10,23 @@ class ClimbBot(bot.SimpleBot):
         msg = str(event.message)
         if msg.startswith("~") or msg.startswith("!"):
             msg_text = msg[1:]
-            self.send_message(event.target, gradeconvert.convert_grade(msg_text))
+            try:
+                if gradeconvert.is_grade(msg_text):
+                    self.send_message(event.target, gradeconvert.convert(msg_text))
+                else:
+                    self.send_message(event.target, "I don't understand: " + msg_text)
+            except StandardError as e:
+                print "Error processing message: " + msg_text, e
 
 
 if __name__ == "__main__":
     # Create an instance of the bot
     # We set the bot's nickname here
     climbbot = ClimbBot("climbbot")
+    climbbot.real_name = "climbbot"
 
     # Let's connect to the host
-    climbbot.connect("irc.snoonet.com", channel="#bottest")
+    climbbot.connect("irc.snoonet.com", channel="#climbing")
 
     # Start running the bot
     climbbot.start()
--- gradeconvert.py
+++ gradeconvert.py
@@ -1,25 +1,166 @@
-import re
+yos={
+    '5.1':'1',
+    '5.2':'2',
+    '5.3':'3',
+    '5.4':'4a',
+    '5.5':'4b',
+    '5.6':'4c',
+    '5.7':'5a',
+    '5.8':'5b',
+    '5.9':'5c',
+    '5.10':'6a-6b+',
+    '5.10a':'6a',
+    '5.10b':'6a+',
+    '5.10c':'6b',
+    '5.10d':'6b+',
+    '5.11':'6c-7a',
+    '5.11a':'6c',
+    '5.11b':'6c',
+    '5.11c':'6c+',
+    '5.11d':'7a',
+    '5.12':'7a+-7c',
+    '5.12a':'7a+',
+    '5.12b':'7b',
+    '5.12c':'7b+',
+    '5.12d':'7c',
+    '5.13':'7c+-8b',
+    '5.13a':'7c+',
+    '5.13b':'8a',
+    '5.13c':'8a+',
+    '5.13d':'8b',
+    '5.14':'8b+-9a',
+    '5.14a':'8b+',
+    '5.14b':'8c',
+    '5.14c':'8c+',
+    '5.14d':'9a',
+    '5.15':'9a+-9c',
+    '5.15a':'9a+',
+    '5.15b':'9b',
+    '5.15c':'9b+',
+    '5.15d':'9c'
+}
 
-yos_grade = re.compile('5\.[1-9]([0-5][abcd]?)?$')
-french_grade = re.compile('([1-3]|[4-5][abcABC]?[+]?|[6-9]([abcABC][+]?)?)$')
-v_grade = re.compile('[vV][bB0-9][0-9]?$')
+french={
+    '1':'5.1',
+    '2':'5.2',
+    '3':'5.3',
+    '4':'5.4',
+    '4a':'5.4',
+    '4b':'5.5',
+    '4+':'5.6',
+    '4c':'5.6',
+    '5':'5.7',
+    '5a':'5.7',
+    '5b':'5.8',
+    '5+':'5.9',
+    '5c':'5.9',
+    '6a':'5.10a',
+    '6a+':'5.10b',
+    '6b':'5.10c',
+    '6b+':'5.10d',
+    '6c':'5.11a',
+    '6c':'5.11b',
+    '6c+':'5.11c',
+    '7a':'5.11d',
+    '7a+':'5.12a',
+    '7b':'5.12b',
+    '7b+':'5.12c',
+    '7c':'5.12d',
+    '7c+':'5.13a',
+    '8a':'5.13b',
+    '8a+':'5.13c',
+    '8b':'5.13d',
+    '8b+':'5.14a',
+    '8c':'5.14b',
+    '8c+':'5.14c',
+    '9a':'5.14d',
+    '9a+':'5.15a',
+    '9b':'5.15b',
+    '9b+':'5.15c',
+    '9c':'5.15d'
+}
 
-def convert_grade(grade):
-    if is_yos(grade):
-        return "The yosemite grade " + grade + " is " + convert_from_yos(grade)
-    elif is_french(grade):
-        return "The french grade " + grade + " is " + convert_from_french(grade)
+v={
+    'V0':'4',
+    'V0+':'4+',
+    'V1':'5',
+    'V2':'5+',
+    'V3':'6A',
+    'V3+':'6A+',
+    'V4':'6B',
+    'V4+':'6B+',
+    'V5':'6C',
+    'V5+':'6C+',
+    'V6':'7A',
+    'V7':'7A+',
+    'V8':'7B',
+    'V8+':'7B+',
+    'V9':'7C',
+    'V10':'7C+',
+    'V11':'8A',
+    'V12':'8A+',
+    'V13':'8B',
+    'V14':'8B+',
+    'V15':'8C',
+    'V16':'8C+'
+}
+
+font={
+    '4':'V0',
+    '4+':'V0+',
+    '5':'V1',
+    '5+':'V2',
+    '6A':'V3',
+    '6A+':'V3+',
+    '6B':'V4',
+    '6B+':'V4+',
+    '6C':'V5',
+    '6C+':'V5+',
+    '7A':'V6',
+    '7A+':'V7',
+    '7B':'V8',
+    '7B+':'V8+',
+    '7C':'V9',
+    '7C+':'V10',
+    '8A':'V11',
+    '8A+':'V12',
+    '8B':'V13',
+    '8B+':'V14',
+    '8C':'V15',
+    '8C+':'V16'
+}
+
+def is_grade(grade):
+    lower_case_grade = grade.lower().replace('f', '')
+    upper_case_grade = grade.upper()
+    return lower_case_grade in yos or \
+           upper_case_grade in v or \
+           lower_case_grade in french or \
+           upper_case_grade in font
+
+def convert(grade):
+    lower_case_grade = grade.lower().replace('f', '')
+    upper_case_grade = grade.upper()
+
+    input_grade = grade
+    conv_grade = None
+    v_grade = None
+
+    if lower_case_grade in yos:
+        conv_grade = "F" + yos[lower_case_grade]
+        #Clean up the input - yosemite grade should always be lower case
+        input_grade = lower_case_grade
+    elif upper_case_grade in v:
+        conv_grade = "font " + v[upper_case_grade]
     else:
-        return "I don't understand: " + grade
+        if lower_case_grade in french:
+            conv_grade = french[lower_case_grade]
+        if upper_case_grade in font:
+            v_grade = font[upper_case_grade]
 
-def is_yos(grade):
-    return bool(yos_grade.match(grade))
-
-def is_french(grade):
-    return bool(french_grade.match(grade))
-
-def convert_from_yos(us_grade):
-    return us_grade
-
-def convert_from_french(french_grade):
-    return french_grade
+    if conv_grade:
+        response = input_grade + " is " + conv_grade
+        if v_grade:
+            response += " or " + v_grade
+        return response
+    raise ValueError('Invalid grade: ' + grade)
